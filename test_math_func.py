326ffcd7f6d21ea7181350c8b5b84cf0f1406464
osboxes.org <muneef97@gmail.com>
2020-01-06 12:45:16 -0500

    initial commit

---
+++ test_math_func.py
@@ -0,0 +1,7 @@
+import math_func
+def test_add():
+	assert math_func.add(7,3) == 10
+	assert math_func.add(4) == 6
+def test_product():
+	assert math_func.product(2,4) == 8
+	assert math_func.product(5) == 10
